87b62d77895fa48c3719b4730a91a16099df5e5f
J. S. Brown <jsbrown7@gmail.com>
2026-02-27 12:47:40 -0800

    Update theme configuration files

--- neovim.lua
+++ neovim.lua
@@ -8,23 +8,23 @@ return {
             colors = {
                 -- Monotone shades (base00-base07)
                 base00 = "#2F3138", -- Default background
-                base01 = "#737373", -- Lighter background (status bars)
+                base01 = "#8C8B86", -- Lighter background (status bars)
                 base02 = "#A06E6E", -- Selection background
-                base03 = "#737373", -- Comments, invisibles
+                base03 = "#8C8B86", -- Comments, invisibles
                 base04 = "#C7C6C1", -- Dark foreground
-                base05 = "#C7C6C1", -- Default foreground
-                base06 = "#C7C6C1", -- Light foreground
-                base07 = "#C7C6C1", -- Light background
+                base05 = "#CBCAC6", -- Default foreground
+                base06 = "#E3E4E0", -- Light foreground
+                base07 = "#E3E4E0", -- Light background
 
                 -- Accent colors (base08-base0F)
-                base08 = "#A06E6E", -- Variables, errors, red
-                base09 = "#A06E6E", -- Integers, constants, orange
-                base0A = "#D9C8B7", -- Classes, types, yellow
-                base0B = "#8A8F6B", -- Strings, green
-                base0C = "#6E91C8", -- Support, regex, cyan
-                base0D = "#4A5A70", -- Functions, keywords, blue
-                base0E = "#5F5FBF", -- Keywords, storage, magenta
-                base0F = "#D9C8B7", -- Deprecated, brown/yellow
+                base08 = "#A07D7D", -- Variables, errors, red
+                base09 = "#AF8D8D", -- Integers, constants, orange
+                base0A = "#D6C8BA", -- Classes, types, yellow
+                base0B = "#8B8E76", -- Strings, green
+                base0C = "#7F99C1", -- Support, regex, cyan
+                base0D = "#7F8DA1", -- Functions, keywords, blue
+                base0E = "#8383C3", -- Keywords, storage, magenta
+                base0F = "#E2D7CC", -- Deprecated, brown/yellow
             },
         },
         config = function(_, opts)
